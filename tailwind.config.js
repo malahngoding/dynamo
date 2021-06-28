b1cabaea35f69c97e51d6e02944d53fb9cd68418
hecterbonha <untungsoandryanto@gmail.com>
2021-06-28 07:44:30 +0700

    adding tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,54 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      white: {
+        DEFAULT: "#ffffff",
+      },
+      black: {
+        DEFAULT: "#18191F",
+        800: "#474A57",
+        600: "#969BAB",
+        400: "#9FA4B4",
+        200: "#EEEFF4",
+        100: "#F4F5F7",
+      },
+      blue: {
+        DEFAULT: "#1947E6",
+        800: "#8094FF",
+        100: "#E9E7FC",
+      },
+      pink: {
+        DEFAULT: "#FF89BB",
+        800: "#FFC7DE",
+        100: "#FFF3F8",
+      },
+      yellow: {
+        DEFAULT: "#FFBD12",
+        800: "#FFD465",
+        100: "#FFF4CC",
+      },
+      green: {
+        DEFAULT: "#00C6AE",
+        800: "#61E4C5",
+        100: "#D6FCF7",
+      },
+      red: {
+        DEFAULT: "#F95A2C",
+        800: "#FF9692",
+        100: "#FFE8E8",
+      },
+    },
+    fontWeight: {
+      normal: 500,
+      bold: 700,
+      black: 900,
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat'],
+      },
+    },
   },
   variants: {
     extend: {},
