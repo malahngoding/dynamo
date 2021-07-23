fe7de005071c1f54d647a0a0be67881e74c4c895
dennyrismanto <rismanto.denny@gmail.com>
2021-07-24 06:19:34 +0700

    Feature Login Fix

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,12 +1,12 @@
 import { FormLogin, ImageLogin } from '@/components/login/login'
 export default function LoginPage() {
     return (
-      <div className="flex flex-col-reverse w-screen min-h-screen bg-green-100 sm:bg-white sm:flex sm:flex-row sm:justify-around sm:items-center sm:w-screen sm:min-h-screen">
-      <div className="-mt-14 z-0 h-full sm:mt-0 sm:flex sm:flex-col sm:justify-end sm:items-center sm:w-1/2 sm:min-h-screen sm:bg-green-100">
+      <div className="flex flex-col-reverse w-screen min-h-screen bg-green-100 lg:bg-white lg:flex lg:flex-row lg:justify-around lg:items-center lg:w-screen lg:min-h-screen">
+      <div className="flex flex-row justify-center items-center -mt-6 xs:-mt-14 sm:-mt-0 z-0 h-full lg:mt-0 lg:flex lg:flex-col lg:justify-end lg:items-center lg:w-1/2 lg:min-h-screen lg:bg-green-100">
       <ImageLogin />
       </div>
 
-      <div className=" sm:flex z-20 sm:flex-col sm:justify-center sm:items-center sm:w-1/2 sm:min-h-screen">
+      <div className="flex flex-row justify-center items-center w-screen h-full lg:flex z-20 lg:flex-col lg:justify-center lg:items-center lg:w-1/2 lg:min-h-screen lg:-ml-0">
       <FormLogin />
       </div>
     </div>
