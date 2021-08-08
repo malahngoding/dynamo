b50deae0d9e5662b8bc82ae8f4e34bd4dd453b8a
dennyrismanto <rismanto.denny@gmail.com>
2021-08-08 10:07:01 +0700

    init

--- pages/dash-example.jsx
+++ pages/dash-example.jsx
@@ -1,7 +1,7 @@
-import { FormProfile, BottomBar } from '@/components/profile/ComponentProfile'
+import Image from '@/components/Image'
 import { NavigationBar } from '@/components/NavBar'
 import { Tags } from '@/components/backup/components/base-component-a/tags'
-export default function LoginPage() {
+export default function DashPage() {
   return (
     <div className="w-full h-screen">
       <div className="inset-x-0 bottom-0 visible h-auto sm:h-0 sm:invisible fixed ">
@@ -41,9 +41,42 @@ export default function LoginPage() {
           <NavigationBar />
         </div>
       </div>
-      <div>
-        <div className="mb-2">
-          <FormProfile />
+
+      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div className="grid gap-5 row-gap-8 lg:grid-cols-2">
+          <div className="flex flex-col justify-center">
+            <div className="max-w-xl mb-6">
+              <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight dark:text-yellow text-gray-800 sm:text-4xl leading-snug sm:leading-normal">
+                <span className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-green to-blue">{`<HelloWorld/>`}</span>
+                <br className="m-2 md:m-6" />
+                Selamat datang di{' '}
+                <span className="relative px-1">
+                  <div className="absolute inset-x-0 bottom-0 h-3 transform -skew-x-12 bg-teal-accent-400" />
+                  <span className="relative inline-block font-black">Malah Ngoding</span>
+                </span>
+              </h2>
+              <p className="text-base text-gray-800 dark:text-white md:text-lg">
+                Belajar koding dan berlatih di platform yang di desain cocok untuk siapa saja.
+                Membaca panduan dan update terbaru. Cocok untuk siapa saja yang bersemangat untuk{' '}
+                <em>ngoding</em>. Semua dalam Bahasa Indonesia.{' '}
+                <span role="img" aria-label="indonesian flag" className="text-xl">
+                  🇮🇩
+                </span>
+              </p>
+              <div>
+                <p>Tes</p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <Image
+              className="max-w-full"
+              src="/static/images/carbon-copy.webp"
+              alt="Carbon Copy Code"
+              width="828"
+              height="686"
+            />
+          </div>
         </div>
       </div>
     </div>
