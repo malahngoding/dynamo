54f9ecec55291251f13b371059c1b68a41e1c25b
hecterbonha <untungsoandryanto@gmail.com>
2021-08-09 20:23:27 +0700

    init

--- pages/api/registration.js
+++ pages/api/registration.js
@@ -5,16 +5,16 @@ export default async function handler(req, res) {
   const { method, body } = req
   switch (method) {
     case 'POST':
-      await detaUser.get(body.email).then((res) => console.log(res))
-      // await detaUser.put(
-      //   {
-      //     name: body.name,
-      //     password: await hashPassword(body.password),
-      //     registeredAt: new Date(),
-      //   },
-      //   body.email
-      // )
-      // res.status(200).json({ status: 'SUCCESS' })
+      // await detaUser.get(body.email).then((res) => console.log(res))
+      await detaUser.put(
+        {
+          name: body.name,
+          password: await hashPassword(body.password),
+          registeredAt: new Date(),
+        },
+        body.email
+      )
+      res.status(200).json({ status: 'SUCCESS' })
       break
     default:
       res.setHeader('Allow', ['GET', 'PUT'])
