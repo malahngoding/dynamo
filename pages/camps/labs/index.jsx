3f84f5664aa04c1bf6bf8bc4229d81608d041c50
hecterbonha <untungsoandryanto@gmail.com>
2021-08-17 17:21:43 +0700

    labs

--- pages/camps/labs/index.jsx
+++ pages/camps/labs/index.jsx
@@ -1,11 +1,35 @@
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { PageSeo } from '@/components/SEO'
 import siteMetadata from '@/data/siteMetadata'
+import { LabsListLayout } from '@/layouts/LabsListLayout'
+import { getAllFilesFrontMatter } from '@/lib/mdx'
 
-export default function Labs() {
+export const POSTS_PER_PAGE = 6
+
+export async function getStaticProps() {
+  const posts = await getAllFilesFrontMatter('labs')
+  const initialDisplayPosts = posts.slice(0, POSTS_PER_PAGE)
+  const pagination = {
+    currentPage: 1,
+    totalPages: Math.ceil(posts.length / POSTS_PER_PAGE),
+    menu: 'labs',
+  }
+
+  return { props: { initialDisplayPosts, posts, pagination } }
+}
+
+export default function Labs({ posts, initialDisplayPosts, pagination }) {
   return (
     <LayoutWrapper>
       <PageSeo title={siteMetadata.titleHandler('Labs')} description={siteMetadata.description} />
+      <LabsListLayout
+        posts={posts}
+        initialDisplayPosts={initialDisplayPosts}
+        pagination={pagination}
+        title="Baca Panduan"
+        subTitle="Intinya Blog Lah"
+        category="labs"
+      />
     </LayoutWrapper>
   )
 }
