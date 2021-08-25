6743f3e1906365bfc17c169b31dad892dacd077e
hecterbonha <untungsoandryanto@gmail.com>
2021-08-25 22:42:46 +0700

    fix index flashcard

--- pages/study/flash-card/index.jsx
+++ pages/study/flash-card/index.jsx
@@ -13,7 +13,6 @@ export default function FlashCardInitiation() {
 
   const [currentQuestionGroup, setCurrentQuestionGroup] = useState(1)
   // GET CURRENT QUESTION GROUP
-  console.log(session)
   useEffect(() => {
     // Ini yang akan dilakukan diambil dari stand
     axios
@@ -31,9 +30,9 @@ export default function FlashCardInitiation() {
         // always executed
       })
 
-  //   // Ini yang akan dilakukan
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [])
+    //   // Ini yang akan dilakukan
+    //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
   return (
     <LayoutWrapper>
       <PageSeo
@@ -43,15 +42,6 @@ export default function FlashCardInitiation() {
       <div className="hidden">
         <UnderConstruction title="Jawab Pertanyaan!" subTitle="Flash Card" />
       </div>
-      {/* <div className="flex flex-col justify-center items-center my-8">
-        <div className="w-[352px] h-[720px] border-2 border-black">
-          <div>Pertanyaan Quiz ke 1</div>
-          <CustomLink href={`/study/flash-card/question?id=${currentQuestionGroup}`}>
-            Menuju Pertanyaan
-          </CustomLink>
-        </div>
-      </div>
-       */}
       <div className="flex flex-col justify-center items-center my-8">
         <QuizDashboardPages />
       </div>
