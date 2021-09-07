a08050c29f6c2f04b0b545bd28ba6bfbfa015ce5
hecterbonha <untungsoandryanto@gmail.com>
2021-09-07 23:07:12 +0700

    chore(data): clear not needed component

--- backup/modal.jsx
+++
@@ -1,9 +0,0 @@
-import { DialogModal } from '@/components/system/modal'
-
-export default function Listings() {
-  return (
-    <div>
-      <DialogModal />
-    </div>
-  )
-}
--- components/backup/components/primitives/seprator.jsx
+++
@@ -1,8 +0,0 @@
-export const Seprator = () => {
-  return (
-    <div className="pt-6 pl-3">
-      <div className="border-t-2 w-[327px] pb-2"></div>
-      <div className="border-t-2 w-[375px]"></div>
-    </div>
-  )
-}
