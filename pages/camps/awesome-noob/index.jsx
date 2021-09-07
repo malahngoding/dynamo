a8cf4de07e34e0eb29e9b3451dc1a2f840a931dc
hecterbonha <untungsoandryanto@gmail.com>
2021-09-07 23:11:16 +0700

    chore(cleanup): cleaning pages

--- pages/camps/awesome-noob/index.jsx
+++ pages/camps/awesome-noob/index.jsx
@@ -3,12 +3,7 @@ import { PageSeo } from '@/components/SEO'
 import siteMetadata from '@/data/siteMetadata'
 import { UnderConstruction } from '@/components/UnderConstruction'
 
-// Cara 1
 export default function AwesomeNoobs() {
-  // 10 Halaman
-  // 1 Tombol kembali hilang
-  // 10 Tombol lanjutkan hilang
-
   return (
     <LayoutWrapper>
       <PageSeo
--- pages/study/flash-card/index.jsx
+++ pages/study/flash-card/index.jsx
@@ -2,11 +2,6 @@ import LayoutWrapper from '@/components/LayoutWrapper'
 import { PageSeo } from '@/components/SEO'
 import { UnderConstruction } from '@/components/UnderConstruction'
 import siteMetadata from '@/data/siteMetadata'
-import CustomLink from '@/components/Link'
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import { useSession } from 'next-auth/client'
-import { QuizDashboardPages } from '@/components/quiz-dashboard'
 
 export default function FlashCardInitiation() {
   return (
@@ -18,18 +13,6 @@ export default function FlashCardInitiation() {
       <div>
         <UnderConstruction title="Jawab Pertanyaan!" subTitle="Flash Card" />
       </div>
-      {/* <div className="flex flex-col justify-center items-center my-8">
-        <div className="w-[352px] h-[720px] border-2 border-black">
-          <div>Pertanyaan Quiz ke 1</div>
-          <CustomLink href={`/study/flash-card/question?id=${currentQuestionGroup}`}>
-            Menuju Pertanyaan
-          </CustomLink>
-        </div>
-      </div> */}
-
-      {/* <div className="flex flex-col justify-center items-center my-8">
-        <QuizDashboardPages />
-      </div> */}
     </LayoutWrapper>
   )
 }
