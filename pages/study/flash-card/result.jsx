f35a815a9873cbc2e68bc8b2d510d7adbbd039ed
Sulthan Taqi Rabbani <sulthanrabbanitaqi@gmail.com>
2021-09-08 23:56:13 +0700

    Update Quiz Score Using getServerSideProps

--- pages/study/flash-card/result.jsx
+++ pages/study/flash-card/result.jsx
@@ -6,27 +6,49 @@ import CustomLink from '@/components/Link'
 import { useRouter } from 'next/router'
 import axios from '@/lib/axios'
 import { QuizResultPages } from '@/components/quiz-score'
+import { useSession } from 'next-auth/client'
+import { getSession } from 'next-auth/client'
 
-export default function FlashCardResults() {
+export default function FlashCardResults(props) {
   const router = useRouter()
   console.log(router.query)
 
-  const handleSubmit = () => {
-    axios
-      // ${router.query.id}
-      .post(`https://opentdb.com/api.php?amount=10`, { totalScore: 1 })
-      .then(function (response) {
-        router.push('/study/flash-card')
-        // handle success
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error)
-      })
-      .then(function () {
-        // always executed
-      })
-  }
+  // useEffect(() => {
+  //   // Ini yang akan dilakukan
+  //   axios
+  //     // ${router.query.id}
+  //     .get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/questions/get/${router.query.id}`)
+  //     .then(function (response) {
+  //       // handle success
+
+  //       axios
+  //         .post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/get-total-score/`, {
+  //           email: session.user.email,
+  //         })
+  //         .then(function (response) {
+  //           // handle success
+  //           setTotalScores(response.data.totalscore)
+  //           console.log(response.data.totalscore)
+  //         })
+  //         .catch(function (error) {
+  //           // handle error
+  //           console.log(error)
+  //         })
+  //         .then(function () {
+  //           // always executed
+  //         })
+  //     })
+  //     .catch(function (error) {
+  //       // handle error
+  //       console.log(error)
+  //     })
+  //     .then(function () {
+  //       // always executed
+  //     })
+
+  //   // Ini yang akan dilakukan
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [])
 
   return (
     <LayoutWrapper>
@@ -43,8 +65,21 @@ export default function FlashCardResults() {
         <CustomLink href="/study/flash-card">Menuju Pertanyaan</CustomLink>
       </div> */}
       <div className="flex flex-col justify-center items-center my-8">
-        <QuizResultPages />
+        <QuizResultPages score={props.score} />
       </div>
     </LayoutWrapper>
   )
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context)
+  const score = await axios.get(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/quiz-result/${session.user.email}`
+  )
+  console.log(score.data[0].nilai)
+  return {
+    props: {
+      score: score.data[0],
+    }, // will be passed to the page component as props
+  }
 }
