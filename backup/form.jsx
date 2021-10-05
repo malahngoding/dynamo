6eff59a6bbb56568c76888a589a1a17603fd5036
hecterbonha <untungsoandryanto@gmail.com>
2021-10-05 17:51:11 +0700

    chore(cleanup): clean up data

--- backup/form.jsx
+++
@@ -1,9 +0,0 @@
-import { LoginForm } from '@/components/forms/form'
-
-export default function FormPage() {
-  return (
-    <div>
-      <LoginForm />
-    </div>
-  )
-}
--- components/backup/components/base-component/slider.jsx
+++
@@ -1,7 +0,0 @@
-export const Slider = () => {
-  return (
-    <div>
-      <input type="range" className="bg-pink" />
-    </div>
-  )
-}
