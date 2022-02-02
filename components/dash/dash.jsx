5bcb0f1221adad87e7a7b1b0b4340c400a801702
dennyrismanto <rismanto.denny@gmail.com>
2022-02-02 19:29:37 +0700

    feat(badge): media-badge and badge detail

--- components/dash/dash.jsx
+++ components/dash/dash.jsx
@@ -3,14 +3,18 @@ import { standService } from '@/lib/service'
 import { useSession } from 'next-auth/react'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import { useBadgeDetail } from '@/modules/dash-nav.store'
+import { CircleX } from 'tabler-icons-react'
+import { MediaBadge } from './media-badge'
 
 export default function Dash() {
   const { data: session, status } = useSession()
   const [sum, setSum] = useState(0)
   const [correct, setCorrect] = useState(0)
   const [incorrect, setIncorrect] = useState(0)
-  // console.log(session, status)
   const token = session.dynamoToken
+  const shown = useBadgeDetail((state) => state.shown)
+  const toggleBadgeDetail = useBadgeDetail((state) => state.toggleBadgeDetail)
 
   useEffect(() => {
     if (status === 'authenticated') {
@@ -59,15 +63,58 @@ export default function Dash() {
   return (
     <div>
       {status === 'authenticated' && (
-        <div>
-          <h1 className="font-black text-4xl mb-2">Selamat Datang!</h1>
-          <h2 className="font-bold text-lg">{session.user.name}</h2>
-          <h2 className="font-bold text-lg">Menjawab Pertanyaan Artikel</h2>
-          <h2 className="font-bold text-lg">Total : {sum} Kali</h2>
-          <h2 className="font-bold text-lg">Benar : {correct}</h2>
-          <h2 className="font-bold text-lg">Salah : {incorrect}</h2>
-        </div>
+        <>
+          <div className="z-0">
+            <h1 className="font-black text-4xl mb-2">Selamat Datang!</h1>
+            <h2 className="font-bold text-lg uppercase">{session.user.name}</h2>
+            <h2 className="font-bold text-lg">Menjawab Pertanyaan Artikel</h2>
+            <h2 className="font-bold text-lg">Total : {sum} Kali</h2>
+            <h2 className="font-bold text-lg">Benar : {correct}</h2>
+            <h2 className="font-bold text-lg">Salah : {incorrect}</h2>
+            <button
+              onClick={() => {
+                toggleBadgeDetail()
+              }}
+            >
+              Badges
+            </button>
+            <BadgeDetail hidden={shown === false ? 'hidden' : 'block'} />
+          </div>
+        </>
       )}
+    </div>
+  )
+}
+
+const BadgeDetail = () => {
+  const tes = [{ tester: '/static/images/badge_one_year.png' }, { media: 'image' }]
+  const shown = useBadgeDetail((state) => state.shown)
+  const toggleBadgeDetail = useBadgeDetail((state) => state.toggleBadgeDetail)
+  return (
+    <div
+      className={`absolute inset-x-0 top-0 w-full bg-black-400 opacity-90 overflow-y-auto my-20 ${
+        shown === false ? 'hidden' : 'block'
+      }`}
+    >
+      <div className="flex justify-end items-end">
+        <button
+          className="rounded-full m-2"
+          onClick={() => {
+            toggleBadgeDetail()
+          }}
+        >
+          <CircleX className="w-[40px] h-[40px]" />
+        </button>
+      </div>
+      <div className="flex justify-center items-center">
+        <MediaBadge media={tes} />
+      </div>
+      <div className="flex justify-center items-center">
+        <p className="uppercase font-bold text-base opacity-100">{'1 year User'}</p>
+      </div>
+      <div className="flex justify-center items-center opacity-100">
+        <p>{'Badge use case #2 - 1-Year User'}</p>
+      </div>
     </div>
   )
 }
